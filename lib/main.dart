aef87c4c13603ee48a1353171871ced784b04667
hyTONG2 <1548327816@qq.com>
2022-06-28 21:06:36 +0800

    拓展组件

--- lib/main.dart
+++ lib/main.dart
@@ -12,35 +12,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, //去除右上角的debug标志
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Counter Expand(HYT)-step3'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -52,64 +34,73 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            //5.3 容器类组件-Transform(变换)
+            Container(
+              color: Colors.deepOrange,
+              child: Transform(
+                alignment: Alignment.topRight, //相对于坐标系原点的对齐方式
+                transform: Matrix4.skewY(0.2), //沿Y轴倾斜0.2弧度
+                child: Container(
+                  padding: const EdgeInsets.all(8.0),
+                  color: Colors.amberAccent,
+                  child: Text('HAHA! THIS IS A COUNTER!',
+                      style: Theme.of(context).textTheme.headline6),
+                ),
+              ),
+            ),
+
+            //3.3 基础组件-图片
+            // 从网络加载图片
+            Align(
+              alignment: Alignment.center,
+              child: Image(
+                image: AssetImage("images/image01.jpg"),
+                width: 70.0,
+                fit: BoxFit.fill,
+              ),
+            ),
+
             const Text(
-              'You have pushed the button this many times:',
+              '\nYou have pushed the button this many times:\n',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+
+            //3.2 基础组件-按钮
+            //3.2.5 带图标的按钮
+            //OutlineButton默认有一个边框，不带阴影且背景透明
+            // 按下后，边框颜色会变亮、同时出现背景和阴影(较弱)
+            OutlinedButton.icon(
+              icon: Icon(Icons.add),
+              label: Text("push"),
+              onPressed: _incrementCounter,
+            ),
           ],
         ),
       ),
+      /*
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+      */
     );
   }
 }
